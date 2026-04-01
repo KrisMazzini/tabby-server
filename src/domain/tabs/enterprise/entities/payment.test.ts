88f43937aefe1ab5018ef8f9f17f0eaee739be0d
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2026-04-01 20:11:04 -0300

    fix: use unique entity id on payment entity tests

--- src/domain/tabs/enterprise/entities/payment.test.ts
+++ src/domain/tabs/enterprise/entities/payment.test.ts
@@ -1,5 +1,8 @@
+import { UniqueEntityId } from '@/core/entities/value-objects/unique-entity-id'
+
 import { InvalidAmountError } from '../../errors/invalid-amount-error'
 import { makeCurrency } from '../../tests/factories/make-currency'
+
 import { Payment } from './payment'
 
 describe('Tabs | Entity: Payment', () => {
@@ -15,15 +18,15 @@ describe('Tabs | Entity: Payment', () => {
 		vi.setSystemTime(new Date('2026-03-28'))
 
 		const payment = Payment.create({
-			payerId: 'payer-1',
-			receiverId: 'receiver-1',
+			payerId: new UniqueEntityId('payer-1'),
+			receiverId: new UniqueEntityId('receiver-1'),
 			amountInCents: 100,
 			currency: makeCurrency({ iso: 'BRL' }),
 			date: new Date(),
 		})
 
-		expect(payment.payerId).toBe('payer-1')
-		expect(payment.receiverId).toBe('receiver-1')
+		expect(payment.payerId.toValue()).toBe('payer-1')
+		expect(payment.receiverId.toValue()).toBe('receiver-1')
 		expect(payment.amountInCents).toBe(100)
 		expect(payment.currency.iso).toBe('BRL')
 		expect(payment.date).toEqual(new Date('2026-03-28'))
@@ -32,8 +35,8 @@ describe('Tabs | Entity: Payment', () => {
 	it('should not be possible to create a payment with an amount less  than or equal to 0', () => {
 		expect(() =>
 			Payment.create({
-				payerId: 'payer-1',
-				receiverId: 'receiver-1',
+				payerId: new UniqueEntityId('payer-1'),
+				receiverId: new UniqueEntityId('receiver-1'),
 				amountInCents: 0,
 				currency: makeCurrency({ iso: 'BRL' }),
 				date: new Date(),
@@ -43,8 +46,8 @@ describe('Tabs | Entity: Payment', () => {
 
 	it('should not be possible to update the amomunt with a value less than or equal to 0', () => {
 		const payment = Payment.create({
-			payerId: 'payer-1',
-			receiverId: 'receiver-1',
+			payerId: new UniqueEntityId('payer-1'),
+			receiverId: new UniqueEntityId('receiver-1'),
 			amountInCents: 100,
 			currency: makeCurrency({ iso: 'BRL' }),
 			date: new Date(),
